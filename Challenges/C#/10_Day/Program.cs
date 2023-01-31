19c1a027ad17ae1d1239fe3dc0be264277b14d19
Daniel Shala <daniel.shala08@gmail.com>
2023-01-31 11:02:56 +0100

    update

--- Challenges/C#/10_Day/Program.cs
+++ Challenges/C#/10_Day/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary<string,int> opcodes = new Dictionary<string, int>() {
+    {"addx",2},
+    {"noop",1},
+};
+
+bool checkStrength(int cycle) {
+    return (cycle == 20 ||
+            cycle == 60 ||
+            cycle == 100 ||
+            cycle == 140 ||
+            cycle == 180 ||
+            cycle == 220);
+}
+
+int getStrength(int cycle, int reg) {
+    return cycle*reg;
+}
+
+
+string[] lines = File.ReadAllLines("input.txt");
+List<string> CRT = new List<string>();
+int X = 1;
+int cycle = 1;
+int lock_ck = 0;
+int sum = 0;
+
+foreach (string line in lines)
+{
+    string op = line.Split()[0];
+    int val = (line.Split().Length > 1) ? Int32.Parse(line.Split()[1]) : 0;
+    lock_ck = opcodes[op];
+
+    do {
+        if(checkStrength(cycle)) {
+            sum += getStrength(cycle, X);
+        }
+        if(cycle%40 >= X-1 && cycle%40 <= X+1) {
+            CRT.Add("#");
+        } else {
+            CRT.Add(".");
+        }
+
+    ++cycle;
+    } while(--lock_ck > 0);
+
+    X += val;
+
+}
+
+Console.WriteLine(cycle);
+Console.WriteLine(sum+"\n");
+
+for(int i = 0; i < 240; i++) {
+    if(i%40==0) Console.WriteLine();
+    Console.Write(CRT[i]);
+}

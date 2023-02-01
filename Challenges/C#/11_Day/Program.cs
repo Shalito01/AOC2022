7fcb5d6ecf5ba8be3bbd00e5620dca11c1b6c4eb
DadyThinkPAd <danielshala00@gmail.com>
2023-02-01 15:09:23 +0100

    [COMPLETED] Day 11 pt1

--- Challenges/C#/11_Day/Program.cs
+++ Challenges/C#/11_Day/Program.cs
@@ -1,85 +1,107 @@
-﻿string[] lines = File.ReadAllLines("input.txt");
+﻿using System.Net.Http.Headers;
+
+string[] lines = File.ReadAllLines("input.txt");
 int monkeyCount = 0;
 List<Monkey> monkeys = new List<Monkey>();
 for(int i = 0; i < lines.Count(); i++) {
     if(lines[i].Equals(string.Format("Monkey {0}:", monkeyCount))) {
-        int monkeyNum = i;
-        int[] items = Array.ConvertAll(lines[++i].Split(":")[1].Split(","), s => int.Parse(s));
+        long[] items = Array.ConvertAll(lines[++i].Split(":")[1].Split(","), s => long.Parse(s));
         string op = lines[++i].Split("=")[1];
         int test = int.Parse(lines[++i].Split(" ").Last());
         int goTrue = int.Parse(lines[++i].Split(" ").Last());
         int goFalse = int.Parse(lines[++i].Split(" ").Last());
 
-        monkeys.Add(new Monkey(items, op, test, goTrue, goFalse));
+        monkeys.Add(new Monkey(monkeyCount, items, op, test, goTrue, goFalse));
         ++monkeyCount;
     }
 }
 
+
+
 for(int i = 0; i < 20; i++) {
-    for(int j = 0; j < monkeys.Count(); ++j) {
-        monkeys[j].inspected += monkeys[j].items.Count();
-        for(int k = 0; k < monkeys[j].items.Count(); ++k) {
-            monkeys[j].update(monkeys[j].items[k]);
-            monkeys[monkeys[j].testItem(monkeys[j].items[k])].addItem(monkeys[j].items[k]);
-            monkeys[j].items.Remove(monkeys[j].items[k]);
+    foreach(Monkey m in monkeys)
+    {
+        while(m.items.TryDequeue(out long oldVal))
+        {
+            m.inspected++;
+            long newVal = m.update(oldVal);
+            newVal /= 3;
+            monkeys[m.testItem(newVal)].addItem(newVal);
         }
+
     }
+
 }
 int max1 = 0, max2 = 0;
 foreach(Monkey m in monkeys) {
+    Console.WriteLine(m.inspected);
     if(m.inspected > max1) {
         max2 = max1;
         max1 = m.inspected;
+    } else if (m.inspected > max2) {
+        max2 = m.inspected;
     }
 }
 
 Console.WriteLine(max1 + " " + max2 + " " + max1*max2);
 
 public class Monkey {
-	public List<int> items;
+    public int id;
+	public Queue<long> items;
 	public string operation;
 	public int test;
-	public int goIfTrue;
-	public int goIfFalse;
+	public int trueDest;
+	public int falseDest;
     public int inspected;
 
-    public Monkey(int[] item, string operation, int test, int goIfTrue, int goIfFalse) {
-        this.items = item.ToList();
+    public Monkey(int id, long[] item, string operation, int test, int trueDest, int falseDest) {
+        this.id = id;
+        items = new Queue<long>(item);
         this.operation = operation;
         this.test = test;
-        this.goIfTrue = goIfTrue;
-        this.goIfFalse = goIfFalse;
-        this.inspected = 0;
+        this.trueDest = trueDest;
+        this.falseDest = falseDest;
     }
 
-    public void addItem(int item) {
-        this.items.Add(item);
+    public void addItem(long item) {
+        this.items.Enqueue(item);
     }
 
-    public void update(int index) {
+    public long update(long oldVal) {
         string op = (operation.Contains("+")) ? "+" : "*";
-        string addend = this.operation.Split(op)[1];
-        int x = index;
-        int y;
-        if(!int.TryParse(addend,out y)) y = x;
-        Console.WriteLine("old: " + index + " : " + y);
+        string addend = operation.Split(op).Last();
+        long newVal = oldVal;
+        long y = (long.TryParse(addend, out long valore)) ? valore : oldVal;
+        Console.Write("old: " + newVal + " "+op+" " + y);
         switch(op) {
-            case "+": x += y;
+            case "+": newVal += y;
                       break;
-            case "*": x *= y;
+            case "*": newVal *= y;
                       break;
         }
-        this.items[this.items.IndexOf(index)] = x;
-
-
+        Console.WriteLine(" = " + newVal);
+        return newVal;
     }
 
-    public int testItem(int index) {
-        int x = index;
-        if (x % test == 0) {
-            return goIfTrue;
-        } else {
-            return goIfFalse;
+    public void prinMonk() {
+        foreach(var item in items) {
+            Console.Write(item+", ");
+        }
+        Console.WriteLine();
+        Console.WriteLine(operation);
+        Console.WriteLine("Divisible by {0}",test);
+        Console.WriteLine("If True: {0} If False: {1}", trueDest,falseDest);
+    }
+
+
+    public int testItem(long testVal) {
+        if (testVal % test == 0)
+        {
+            return trueDest;
+        }
+        else
+        {
+            return falseDest;
         }
     }
 }
